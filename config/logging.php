900921433b2723dcafff89c6eb44ea8961c23a63
Markus Corvinus <markus.corvinus0827@gmail.com>
2024-10-09 13:08:03 +0800

    fix logging error-permission denied

--- config/logging.php
+++ config/logging.php
@@ -52,6 +52,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 30,
+            'permission' => 0664, 
         ],
 
         'slack' => [
